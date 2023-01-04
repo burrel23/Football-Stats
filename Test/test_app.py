c1bab82c2fe6aed58344c85d94542db2f0cdf7df
burrel23 <pnbloic@gmail.com>
2023-01-04 21:56:18 +0100

    test_app

--- Test/test_app.py
+++ Test/test_app.py
@@ -7,6 +7,14 @@ def client():
     client = app.test_client()
     yield client
 
+def test_welcome_page(client):
+    response = client.get('/')
+    assert b"<body>Bienvenue sur Football Stats</body>" in response.data
+
+def test_teams(client):
+    response = client.get('/teams')
+    assert response.status_code == 200
+    
 def test_players(client):
     response = client.get('/players')
     assert response.status_code == 200
