c78baa707b6a8a5a6e9d6875ba7950113f185a9c
burrel23 <pnbloic@gmail.com>
2023-01-08 13:47:28 +0100

    edit test app and templates

---
+++ test_app.py
@@ -0,0 +1,20 @@
+import pytest
+from app import app
+
+@pytest.fixture
+def client():
+    app.config['TESTING'] = True
+    client = app.test_client()
+    yield client
+
+def test_welcome_page(client):
+    response = client.get('/')
+    assert b"Welcome on Football stats" in response.data
+
+def test_teams(client):
+    response = client.get('/teams')
+    assert response.status_code == 200
+    
+def test_players(client):
+    response = client.get('/players')
+    assert response.status_code == 200
