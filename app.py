6b9acea846fb0262adfdcb9c49675636ca24743a
burrel23 <pnbloic@gmail.com>
2023-01-08 11:15:34 +0100

    add modules and edit error.html

--- app.py
+++ app.py
@@ -1,6 +1,9 @@
 import http.client
+import json
 import sys
+import urllib
 from flask import Flask, render_template, request, redirect, url_for
+from json2html import json2html
 from functions.player_data import * 
 
 app = Flask(__name__)
@@ -18,44 +21,44 @@ def teams():
 
 @app.route('/team_stats', methods=['GET', 'POST'])
 def team_stats():
-    try:
-        team_name = urllib.parse.quote_plus(request.args['team_name'])
-        league_name = urllib.parse.quote_plus(request.args['league_name'])
-        season = request.args['season'].strip()
+    # try:
+    team_name = urllib.parse.quote_plus(request.args['team_name'])
+    league_name = urllib.parse.quote_plus(request.args['league_name'])
+    season = request.args['season'].strip()
 
-        conn = http.client.HTTPSConnection("v3.football.api-sports.io")
+    conn = http.client.HTTPSConnection("v3.football.api-sports.io")
 
-        headers = {
-            'x-rapidapi-host': "v3.football.api-sports.io",
-            'x-rapidapi-key': "45489aa3142e552f28903cd2fb5e92de"
-        }
+    headers = {
+        'x-rapidapi-host': "v3.football.api-sports.io",
+        'x-rapidapi-key': "45489aa3142e552f28903cd2fb5e92de"
+    }
 
-        conn.request("GET", "/teams?name=" + team_name, headers=headers)
+    conn.request("GET", "/teams?name=" + team_name, headers=headers)
 
-        res = conn.getresponse()
-        data = res.read()
+    res = conn.getresponse()
+    data = res.read()
 
-        team_id = json.loads(data)["response"][0]["team"]["id"]
+    team_id = json.loads(data)["response"][0]["team"]["id"]
 
-        conn.request("GET", "/leagues?name=" + league_name, headers=headers)
+    conn.request("GET", "/leagues?name=" + league_name, headers=headers)
 
-        res = conn.getresponse()
-        data = res.read()
+    res = conn.getresponse()
+    data = res.read()
 
-        league_id = json.loads(data)["response"][0]["league"]["id"]
+    league_id = json.loads(data)["response"][0]["league"]["id"]
 
-        conn.request("GET",
-                     "/teams/statistics?league=" + str(league_id) + "&season=" + season + "&team=" + str(team_id),
-                     headers=headers)
+    conn.request("GET",
+                    "/teams/statistics?league=" + str(league_id) + "&season=" + season + "&team=" + str(team_id),
+                    headers=headers)
 
-        res = conn.getresponse()
-        data = res.read()
+    res = conn.getresponse()
+    data = res.read()
 
-        stats = json.loads(data)["response"]
+    stats = json.loads(data)["response"]
 
-        return json2html.convert(json=stats)
-    except:
-        return render_template("error.html")
+    return json2html.convert(json=stats)
+    # except:
+    #     return render_template("error.html")
 
 
 @app.route('/players', methods = ["POST","GET"])
